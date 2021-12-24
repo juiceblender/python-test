ae1270701f6c4adcebe6674931ceac2da67d9c4b
Lerh Low <llow@seek.com.au>
2021-12-24 12:57:30 +1100

    Some changes 🤔

--- main.py
+++ main.py
@@ -19,19 +19,21 @@ def load_column_into_list(file_path: str, column_number: int = 0) -> List[str]:
 
         return data
 
-target = 'casual'
+target = '[Cc]asual'
 n = 3
 
+
 def search(text: str, target: str, n: int) -> object:
-    #Searches for text,and retrieves n words either side of the text,which are returned seperatly
+    #Searches for text,and retrieves n words either side of the text,which are returned seperately
     word = r"\W*([\w]+)"
-    groups = re.search(r'{}\W*{}{}'.format(word * n,'casual',word * n),text).groups()
-    print(groups[:n], groups[n:])
+    groups = re.search(r'{}\W*{}{}'.format(word * n, target, word * n), text).groups()
+    return groups[:n], groups[n:]
 
-    run_search = data.apply(lambda row: search(row['text'],target,n),
-            axis=1)
+    #run_search = data.apply(lambda row: search(row['text'],target,n),
+    #        axis=1)
+    # run_search = [(lambda row: search(row['text'],target,n))(row) for row in data]
 
-    print(run_search)
+    # print(run_search)
 
 data = load_column_into_list("data.csv")
 
@@ -41,7 +43,7 @@ with open("output.csv", "w") as csv_file:
 
     for text in data:
         result = search(text,target,n)
-        csv_writer.writerow([text, result[text,1]])
+        csv_writer.writerow([text, result[0], result[1]])
         print(f"text: {text}, result: {result}")
 
     csv_file.close()
